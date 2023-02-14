80438b9dedb7e186ca30d486a229c4edf965d065
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-02-14 01:25:16 -0300

    delete astro project

--- src/utils/scroll-to-element.ts
+++
@@ -1,9 +0,0 @@
-export function scrollToElement(element: string) {
-  const el = document.getElementById(element.trim());
-  const position = el?.getBoundingClientRect();
-  window.scrollTo({
-    behavior: "smooth",
-    left: position!.left,
-    top: position!.top + window.scrollY - 100,
-  });
-}
