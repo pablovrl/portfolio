1311006e8d56afc7b2209569812b15b5b2eb3c4e
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-02-14 01:46:29 -0300

    add website data

---
+++ data/contacts.ts
@@ -0,0 +1,23 @@
+export const CONTACTS = [
+  {
+    name: "Mail",
+    url: "mailto:pablovillarroel135@gmail.com",
+    icon: "gmail",
+    user: "pablovillarroel135@gmail.com",
+    color: "bg-yellow-100"
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/pablo-villarroel-antillanca-850974186/",
+    icon: "linkedin",
+    user: "Pablo Villarroel Antillanca",
+    color: "bg-pink-100"
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/pablovrl",
+    icon: "github",
+    user: "@pablovrl",
+    color: "bg-purple-100"
+  }
+];
---
+++ data/projects.ts
@@ -0,0 +1,22 @@
+export const PROJECTS = [
+  {
+    title: "Nestjs API Store",
+    image: "api-store.png",
+    description: {
+      en: "API developed with Nestjs. It's the back-end of a store. It has a user system, products, categories, carts and JWT authentication. Currently in development.",
+      es: "API desarrollada con Nestjs. Es el back-end de una tienda. Tiene un sistema de usuarios, productos, categorías, carritos y autenticación con JWT. Actualmente en desarrollo."
+    },
+    codeUrl: "https://github.com/pablovrl/nestjs-store-api",
+    demoUrl: "https://nestjs-store-api-production.up.railway.app/api",
+    wip: true,
+  },
+  {
+    title: "Mi Portafolio UBB",
+    image: "mi-portafolio-ubb.png",
+    description: {
+      en: "Website for the Computer Science students at the University of Bío-Bío. In which they can create and share a digital portfolio. Also employers can search students by their skills and contact them.",
+      es: "Sitio web para los estudiantes de Ingeniería en Informática de la Universidad de Bío-Bío. En el cual pueden crear y compartir un portafolio digital. También los empleadores pueden buscar estudiantes por sus habilidades y contactarlos."
+    },
+    codeUrl: "https://github.com/pablovrl/mi-portafolio-ubb",
+  },
+];
