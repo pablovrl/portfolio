e0991e422eb4dc7d3b0e6ad59e2ea681fe9d406b
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-02-15 00:18:59 -0300

    add Project section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
+import { Projects } from "./components/Projects";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <div className="px-4 pt-20">
+      <div className="px-4 pt-20 flex flex-col gap-8">
         <Home />
+        <Projects />
       </div>
     </div>
   );
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,10 +1,14 @@
-export const PROJECTS = [
+import { Project } from "../types";
+import apiStoreImage from "../assets/api-store.png";
+import miPortafolioUbbImage from "../assets/mi-portafolio-ubb.png";
+
+export const PROJECTS: Project[] = [
   {
     title: "Nestjs API Store",
-    image: "api-store.png",
+    image: apiStoreImage,
     description: {
       en: "API developed with Nestjs. It's the back-end of a store. It has a user system, products, categories, carts and JWT authentication. Currently in development.",
-      es: "API desarrollada con Nestjs. Es el back-end de una tienda. Tiene un sistema de usuarios, productos, categorías, carritos y autenticación con JWT. Actualmente en desarrollo."
+      es: "API desarrollada con Nestjs. Es el back-end de una tienda. Tiene un sistema de usuarios, productos, categorías, carritos y autenticación con JWT. Actualmente en desarrollo.",
     },
     codeUrl: "https://github.com/pablovrl/nestjs-store-api",
     demoUrl: "https://nestjs-store-api-production.up.railway.app/api",
@@ -12,10 +16,10 @@ export const PROJECTS = [
   },
   {
     title: "Mi Portafolio UBB",
-    image: "mi-portafolio-ubb.png",
+    image: miPortafolioUbbImage,
     description: {
       en: "Website for the Computer Science students at the University of Bío-Bío. In which they can create and share a digital portfolio. Also employers can search students by their skills and contact them.",
-      es: "Sitio web para los estudiantes de Ingeniería en Informática de la Universidad de Bío-Bío. En el cual pueden crear y compartir un portafolio digital. También los empleadores pueden buscar estudiantes por sus habilidades y contactarlos."
+      es: "Sitio web para los estudiantes de Ingeniería en Informática de la Universidad de Bío-Bío. En el cual pueden crear y compartir un portafolio digital. También los empleadores pueden buscar estudiantes por sus habilidades y contactarlos.",
     },
     codeUrl: "https://github.com/pablovrl/mi-portafolio-ubb",
   },
