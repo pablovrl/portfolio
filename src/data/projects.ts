e29880a0a8c698cafebd5e9e40b0caded7959b9c
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-02-23 21:55:20 -0300

    edit projects data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,18 +1,28 @@
 import { Project } from "../types";
 import apiStoreImage from "../assets/api-store.png";
 import miPortafolioUbbImage from "../assets/mi-portafolio-ubb.png";
+import pixelMeImage from "../assets/pixel-me.png";
 
 export const PROJECTS: Project[] = [
+  {
+    title: "Pixel Me",
+    image: pixelMeImage,
+    description: {
+      en: "Web app to pixelate selected areas of an image. It usses the cloudinary API to upload and process the images.",
+      es: "Aplicación web para pixelar áreas seleccionadas de una imagen. Usa la API de cloudinary para subir y procesar las imágenes.",
+    },
+    codeUrl: "https://github.com/pablovrl/pixelme",
+    demoUrl: "https://pixel-me.vercel.app/",
+  },
   {
     title: "Nestjs API Store",
     image: apiStoreImage,
     description: {
-      en: "API developed with Nestjs. It's the back-end of a store. It has a user system, products, categories, carts and JWT authentication. Currently in development.",
-      es: "API desarrollada con Nestjs. Es el back-end de una tienda. Tiene un sistema de usuarios, productos, categorías, carritos y autenticación con JWT. Actualmente en desarrollo.",
+      en: "API developed with Nestjs. It's the back-end of a store. It has a user system, products, categories, carts and role based auth.",
+      es: "API desarrollada con Nestjs. Es el back-end de una tienda. Tiene un sistema de usuarios, productos, categorías, carritos y autenticación basada en roles.",
     },
     codeUrl: "https://github.com/pablovrl/nestjs-store-api",
     demoUrl: "https://nestjs-store-api-production.up.railway.app/api",
-    wip: true,
   },
   {
     title: "Mi Portafolio UBB",
